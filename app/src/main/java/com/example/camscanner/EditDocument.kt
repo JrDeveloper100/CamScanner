d0cd0d98cdccd9e4218cfa1007e86f7b462fbcbb
JrAppDeveloper <jrappdeveloper.cdg@gmail.com>
2023-08-02 16:01:16 +0500

    Backend Edit Document Section Completed

--- app/src/main/java/com/example/camscanner/EditDocument.kt
+++ app/src/main/java/com/example/camscanner/EditDocument.kt
@@ -1,19 +1,20 @@
 package com.example.camscanner
 
 import android.content.Context
-import android.content.Intent
 import android.content.res.Configuration
+import android.graphics.Bitmap
 import android.graphics.BitmapFactory
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Canvas
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
 import android.view.View
-import android.widget.FrameLayout
 import android.widget.ImageView
 import android.widget.LinearLayout
+import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintLayout
-import androidx.fragment.app.Fragment
 import com.google.android.material.appbar.MaterialToolbar
+import com.xiaopo.flying.sticker.StickerView
 import ja.burhanrashid52.photoeditor.PhotoEditorView
 import java.util.Stack
 
@@ -84,6 +85,8 @@ class EditDocument : AppCompatActivity() {
     private lateinit var btnWatermark4Colors : LinearLayout
     private lateinit var btnWatermark4Opacity : LinearLayout
     private lateinit var photoEditorView : PhotoEditorView
+    private lateinit var stickerView : StickerView
+    private lateinit var imageLayout : ConstraintLayout
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_document)
@@ -150,7 +153,9 @@ class EditDocument : AppCompatActivity() {
         btnEraser = bottomSheetLayoutColorHighlight.findViewById(R.id.btnEraser)
         btnColorInner = bottomSheetLayoutColorHighlight.findViewById(R.id.btnColorInner)
         photoEditorView = findViewById(R.id.photoEditorView)
+        stickerView = findViewById(R.id.stickerView)
         photoEditorView.source.setImageBitmap(bitmapImage)
+        imageLayout = findViewById(R.id.imageLayout)
 
 //        btnColorFilter = findViewById(R.id.btnColorFilter)
 //        btnAdjust = findViewById(R.id.btnAdjust)
@@ -293,6 +298,7 @@ class EditDocument : AppCompatActivity() {
 //            btnWatermark4Opacity.setBackgroundResource(R.drawable.edit_document_bottom_icon_design_light_mode)
 //        }
 
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -305,8 +311,70 @@ class EditDocument : AppCompatActivity() {
         return true
     }
 
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            android.R.id.home -> {
+                onBackPressed() // Go back to the previous activity
+                true
+            }
+            R.id.done -> {
+                goToActivity()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun goToActivity() {
+         Constant.original = getMainFrameBitmap()
+    }
+
+    private fun getMainFrameBitmap(): Bitmap? {
+        val createBitmap = Bitmap.createBitmap(imageLayout.width, imageLayout.height, Bitmap.Config.ARGB_8888)
+        imageLayout.draw(Canvas(createBitmap))
+        return imageBitmap(createBitmap)
+    }
+
+    private fun imageBitmap(bitmap: Bitmap) : Bitmap? {
+
+        val width = bitmap.width
+        var i = -1
+        var height = bitmap.height
+        var i2 = -1
+        var i3 = width
+        var i4 = 0
+        while (i4 < bitmap.height) {
+            var i5 = i2
+            var i6 = i3
+            for (i7 in 0 until bitmap.width) {
+                if (bitmap.getPixel(i7, i4) shr 24 and 255 > 0) {
+                    if (i7 < i6) {
+                        i6 = i7
+                    }
+                    if (i7 > i) {
+                        i = i7
+                    }
+                    if (i4 < height) {
+                        height = i4
+                    }
+                    if (i4 > i5) {
+                        i5 = i4
+                    }
+                }
+            }
+            i4++
+            i3 = i6
+            i2 = i5
+        }
+        return if (i < i3 || i2 < height) {
+            null
+        } else {
+            Bitmap.createBitmap(bitmap, i3, height, i - i3 + 1, i2 - height + 1)
+        }
+    }
+
     // Function to check if dark mode is enabled
-    fun isDarkModeEnabled(context: Context): Boolean {
+    private fun isDarkModeEnabled(context: Context): Boolean {
         val configuration = context.resources.configuration
         return (configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK) == Configuration.UI_MODE_NIGHT_YES
     }
